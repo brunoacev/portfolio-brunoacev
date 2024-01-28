d100747ea46c000cb409048fbd9e19bbd5c70a8f
Bruno Azevedo <brunoacev@gmail.com>
2024-01-27 21:24:03 -0300

    Fix: Remove imports from home page about previus journey component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import { AboutMe } from "@/components/about-me";
 import { Journey } from "@/components/journey";
-import { PersonalJourney } from "@/components/personal-journey";
-import { TimeContextProvider } from "@/context/time-card-context";
 
 export default function Home() {
   return (
