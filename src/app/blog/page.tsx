aac2284c9ca072682aee58c226a14da45736ddc7
Bruno Azevedo <brunoacev@gmail.com>
2024-02-05 15:50:07 -0300

    Feat: updating the description of the blog content

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,12 +5,16 @@ export default function BlogPage() {
   return (
     <main className="h-full w-full p-4 flex flex-col gap-4">
       <section className="flex flex-col gap-4">
-        <h1 className="text-2xl md:text-4xl font-bold">
+        <h1 className="text-2xl md:text-6xl font-bold">
           Explore os ultimos updates
         </h1>
         <p className="text-sm font-semibold text-zinc-600 dark:text-zinc-400">
-          Estou ansioso para compartilhar insights e informações fascinantes
-          sobre o vasto mundo da tecnologia neste espaço dedicado ao blog.
+          Mal posso esperar para compartilhar com você, estimado leitor, uma
+          profusão de insights instigantes e informações fascinantes sobre o
+          vasto e dinâmico universo da tecnologia. Este espaço dedicado ao blog
+          servirá como um canal exclusivo para explorarmos juntos as tendências
+          mais recentes, inovações emocionantes e perspectivas esclarecedoras
+          que moldam o cenário tecnológico atual.
         </p>
       </section>
 
