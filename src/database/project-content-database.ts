a11c2484118e68b733c50e882c7eb1613bd716cd
Bruno Azevedo <brunoacev@gmail.com>
2024-06-10 16:33:56 -0300

    Fix: Att about time and stack content project

--- src/database/project-content-database.ts
+++ src/database/project-content-database.ts
@@ -16,19 +16,12 @@ const projectContentData: Project[] = [
     title: "War Games",
     subtitle: "Private Cabal Server",
     description:
-      "Private server MMORPG, Classic Cabal Game and Free To Play. Projeto esta em periodo de desenvolvimento, com data de lançamento para Julho 2024.",
+      "Private server MMORPG, Classic Cabal Game and Free To Play. Projeto esta em periodo de desenvolvimento, com data de lançamento para Setembro 2024.",
     urlToPreview: "https://war-games-demo.vercel.app/",
     urlToGithub: "https://github.com/brunoacev/war-games-demo",
-    date: "Setembro de 2023 - Atualmente",
+    date: "Setembro de 2023 - Outubro de 2023",
     image: "/logos/wargames.png",
-    stack: [
-      "Freelancer",
-      "NextJS",
-      "Stripe",
-      "Tailwind",
-      "PostgreSQL",
-      "Docker",
-    ],
+    stack: ["Freelancer", "NextJS", "Stripe", "Tailwind"],
   },
 ];
 
